83df649f3e44a97c961349551e8e4a3a37183029
Kivy <tuankiet.thptcamau@gmail.com>
2024-05-27 01:53:42 +0700

    Backend for shopapp

---
+++ src/main/java/com/project/shopapp/responses/BaseResponse.java
@@ -0,0 +1,24 @@
+package com.project.shopapp.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data//toString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseResponse {
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+}
